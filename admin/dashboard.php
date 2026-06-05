7c2a62d6b8e156088e8a0a3b2ddde45cf086a628
KevinCristian24 <zodiacvirgo01@gmail.com>
2026-06-05 22:14:40 +0700

    alur baru

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,10 @@
 <?php
 
 include '../koneksi.php'; //inisiasi testing dashboard
-// abfeafa
+// Test git branch
+// 1. (terminal)
+// 2. git branch
+// 3. git checkout fitur-dashboard
+// 4. git status
+// 5. git add admin/dashboard.php
 ?>
